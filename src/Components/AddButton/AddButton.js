f3c6aa2191b0639606a97b925d1409358984cb6f
leonardo Kaplan <kaplanleonardo@gmail.com>
2020-01-06 16:08:58 -0600

    designing components

---
+++ src/Components/AddButton/AddButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AddButton.css';
+
+class AddButton extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+       <div className="AddButton">
+         <p>add button</p>
+       </div>
+    );
+  }
+};
+
+export default AddButton;
