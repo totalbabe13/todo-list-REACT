f794377312db5769d23ff0cfb3230608f5832321
leonardo Kaplan <kaplanleonardo@gmail.com>
2020-01-06 16:39:40 -0600

    building lay out css

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import AddButton from '../AddButton/AddButton';
 
 function App() {
   return (
     <div className="App">
+      <div className="conatiner-one ">
+        <div className="container-two">
+        </div>
+      </div>
 
     </div>
   );
